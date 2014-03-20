e63d65e01522c955363f12523024f2bb7f1f8909
Liao Michelle <g3michhy@cdf.toronto.edu>
2014-03-20 17:04:29 -0400

    index done

--- application/views/template/index.php
+++ application/views/template/index.php
@@ -1,10 +1,10 @@
 
 <?php 
 
-if ($this->session->userdata('is_logged_in')){
-	redirect(candystore/index);
-}else{
-	$this->load->view("template/login_form.php");
-}
+	if ($this->session->userdata('is_logged_in')){
+			redirect('candystore/index', 'refresh');
+	}else{
+		$this->load->view("template/login_form.php");
+	}
 
 ?>
